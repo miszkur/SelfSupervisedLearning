00b85062e8ba0de293ee6fd4c6b4a263056b3c9d
miszkur <miszkurka.aga@gmail.com>
2021-12-07 14:35:53 +0100

    add script for direct pred with deeeper projection

--- experiments/scripts/direct_pred_simclr.py
+++ experiments/scripts/direct_pred_simclr.py
@@ -12,9 +12,9 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--epochs', type=int, default=101)
     parser.add_argument(
-        '--path_to_save_encoder', type=str, default='saved_model/direct_pred.h5')
+        '--path_to_save_encoder', type=str, default='saved_model/direct_pred_deeper_projection.h5')
     args = parser.parse_args()
-    config = conf.get_direct_pred()
+    config = conf.get_deeper_projection()
     ds, _ = get_cifar10(batch_size=config.batch_size, split='train')
     experiment = eu.Experiment(config)
     experiment.train(
