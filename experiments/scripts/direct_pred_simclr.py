80ea784a42863ae72f76799b7e3db9424ce195ce
miszkur <miszkurka.aga@gmail.com>
2021-12-08 13:22:10 +0100

    save projection for simclr experiment

--- experiments/scripts/direct_pred_simclr.py
+++ experiments/scripts/direct_pred_simclr.py
@@ -12,7 +12,9 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--epochs', type=int, default=101)
     parser.add_argument(
-        '--path_to_save_encoder', type=str, default='saved_model/direct_pred_deeper_projection.h5')
+        '--path_to_save_encoder', type=str, default='saved_model/deeper_projection_encoder.h5')
+    parser.add_argument(
+        '--path_to_save_projector', type=str, default='saved_model/deeper_projection_projector.h5')
     args = parser.parse_args()
     config = conf.get_deeper_projection()
     ds, _ = get_cifar10(batch_size=config.batch_size, split='train')
@@ -20,6 +22,7 @@ def main():
     experiment.train(
         ds, 
         saved_encoder_path=args.path_to_save_encoder, 
+        saved_projection_head_path=args.path_to_save_projector,
         epochs=args.epochs)
 
 if __name__ == '__main__':
